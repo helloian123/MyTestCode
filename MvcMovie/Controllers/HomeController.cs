a36751c629e3e9d40a00dc143ff7f97354d0a52c
helloian123 <helloian123@sina.com>
2022-08-08 10:45:04 +0800

    33

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -33,5 +33,12 @@ namespace MvcMovie.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
     }
 }
